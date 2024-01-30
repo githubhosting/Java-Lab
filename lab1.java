a9ba8f55ba43e208e47240b50ec3ef3baffd3450
myselfshravan <myselfshravan@gmail.com>
2024-01-30 12:56:28 +0530

    lab 1 a changed

--- lab1.java
+++ lab1.java
@@ -1,17 +1,41 @@
-//Area of a rectangle using Java
 import java.util.Scanner;
 
 public class lab1a {
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the length of the rectangle :");
-    double length = scanner.nextDouble();
-    System.out.println("Enter the width of the rectangle :");
-    double width = scanner.nextDouble();
-    System.out.println("The area of the rectangle is " + length * width);
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter coefficient 'a': ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter coefficient 'b': ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Enter coefficient 'c': ");
+        double c = scanner.nextDouble();
+
+        double determinant = b * b - 4 * a * c;
+
+        if (determinant > 0) {
+            double root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+            System.out.println("Root 1: " + root1 + " and Root 2: " + root2);
+        }
+        else if (determinant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Root: " + root);
+        }
+        else {
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-determinant) / (2 * a);
+            System.out.println("Root 1: " + realPart + " + " + imaginaryPart + "i");
+            System.out.println("Root 2: " + realPart + " - " + imaginaryPart + "i");
+        }
+
+        scanner.close();
+    }
 }
+
 
 //Multiply Two Numbers
 import java.util.Scanner;

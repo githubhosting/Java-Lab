a9874d6897ee73f659b3a2625b0caab966c9cd19
myselfshravan <myselfshravan@gmail.com>
2024-01-30 13:26:36 +0530

    2

--- lab2.java
+++ lab2.java
@@ -1,3 +1,9 @@
+// 2.	Write a java program to create an Employee database which stores following information about the Employee: Name, Emp_id, department, age, and designation. Perform the following operations:
+// a.	Read and display the details of at least five Employees.
+// b.	Calculate and display the sum of salary of all the employees of “sales” department.
+// c.	Retrieve the details of “highest paid manager” in the purchase department.
+
+
 import java.util.Scanner;
 
 class Employee {
